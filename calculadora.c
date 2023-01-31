cc60a23bb4cd494f86c421dcb2e73e23c259f6f2
João Victor Carniel <joaovictorcarniel.cert.trabalho@gmail.com>
2023-01-31 19:09:52 -0300

    adicionar soma

--- calculadora.c
+++ calculadora.c
@@ -1,1 +1,31 @@
-dasdasdasdasda
+#include <stdio.h>
+
+int main()
+{
+  int x = 0, y = 0, z = 0;
+
+  int menu;
+
+  printf("Digite o numero da opção que voce deseja usar na calculadora de 2 FATORES... \n");
+  printf("(1) Para calcular uma operação de Adição.\n");
+  printf("(2) Para calcular uma operação de Subtração.\n");
+  printf("(3) Para calcular uma operação de Multiplicação.\n");
+  printf("(4) Para calcular uma operação de Divisão.\n");
+  printf("(5) Encerrar o programa.\n");
+
+  scanf("%d", &menu);
+
+  switch (menu)
+  {
+  // Opcão 1 *Adição*
+  case 1:
+    printf("Introduza uma número Válido\n");
+    scanf("%d", &x);
+    printf("Introduza uma número Válido\n");
+    scanf("%d", &y);
+    z = x + x;
+    printf("A soma é:%d\n", z);
+    break;
+  }
+  return 0;
+}
